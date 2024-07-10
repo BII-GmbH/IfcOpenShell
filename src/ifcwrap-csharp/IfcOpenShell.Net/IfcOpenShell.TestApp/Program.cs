595cc310c918cd7d5b1af3bf71f7fcad8248b798
Wilhelm Schnepp <53336967+wschnepp@users.noreply.github.com>
2024-07-10 18:25:38 +0200

    test program for benchmarking

--- src/ifcwrap-csharp/IfcOpenShell.Net/IfcOpenShell.TestApp/Program.cs
+++ src/ifcwrap-csharp/IfcOpenShell.Net/IfcOpenShell.TestApp/Program.cs
@@ -1,51 +1,79 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using System.Collections.Immutable;
+using System.Diagnostics;
 using IfcOpenShell;
 
 
 Console.WriteLine("Hello, World!");
+var i = Console.Read();
 
-const string fileName = "C:\\LocalDirs\\schnepp\\IfcOpenShell-Progress\\sampleHouse.ifc";
+const string fileName = "I:\\MPAL VA Gleise + IRA-Bahnsteig_1023-1.IFC";
 
 using var model = ifcopenshell_net.open(fileName);
-
-var wall = model.instances_by_type("IfcWall").First();
-var elem = model.instance_by_guid("3w0zWKm7n8SB1qbfwUzt0J");
-var pset = elem.GetPsets();
-Console.WriteLine(wall);
 
 var settings = new IteratorSettings();
 settings.set((ulong)IteratorSettings.Setting.GENERATE_UVS, true);
 settings.set((ulong)IteratorSettings.Setting.WELD_VERTICES, false);
 settings.set((ulong)IteratorSettings.Setting.NO_NORMALS, false);
 
-// var wall = model.instances_by_type("IfcWall")[0];
-// var name = wall.TryGetAttribute("Name").TryGetAsString().TryGetValue(out var nameAtt) ? nameAtt : "None";   
-// Console.WriteLine("Name: " + name);
-//
-// var name2 = wall
-//     .TryGetAttribute("Namelaskj", out var name2Attribute) && 
-//     name2Attribute.TryGetAsString().HasValue() ? name2Attribute.TryGetAsString().getValue() : "None";
-// Console.WriteLine("Namelaskj: " + name2);
-Console.WriteLine($"Ifc {model.schema_name()} has map conversion implemented natively: {!model.schema_name().Equals("IFC2X3")}");
+Stopwatch watch = Stopwatch.StartNew();
+Console.WriteLine($"Importing with {Environment.ProcessorCount}");
+var iterator = new Iterator(settings, model, Environment.ProcessorCount);
 
-// var ifcMapConversion = model.instances_by_type("IfcMapConversion")?.FirstOrDefault();
-//             
-// if (ifcMapConversion != null) {
-//                 
-//     var mapConversion = ifcMapConversion.get_attribute_names();  
-//     foreach (var attribute in mapConversion) {
-//         Console.WriteLine($"Found attribute on map conversion: {attribute}");
-//     }
-//             
-//     //var eastings = ifcMapConversion.TryGetAttribute("XAxisOrdinate").TryGetAsDouble().HasValue() ? ifcMapConversion.TryGetAttribute("XAxisOrdinate").TryGetAsDouble().GetValue() : -1.0;
-//     //eastings.second
-//                     
-//     //Console.WriteLine($"XAxisOrdinate: {eastings}");
-// }
-                
-// var projectedCrs = Option.SomeIfNotNull(model.instances_by_type("IfcProjectedCRS"))
-//     .FlatMap(l => l.FirstOrNone());
-// if (projectedCrs.TryGetValue(out var projected)) { Debug.Log($"Found projected CRS: {projected}"); }
+if (iterator.initialize()) {
+    while (true) {
+        Console.WriteLine($"Progress: {iterator.progress()} after {watch.Elapsed}");
+        // TODO: figure out if this can help us get accurate bounding boxes for the products
+        //iterator.compute_bounds()
+        var elem = iterator.get();
+
+        var productId = elem.product().Guid() ?? "(Unknown)";
+
+        var elem_instance = elem.product();
+        var product = elem.product();
+        
+        var productPropertyInfo = ImmutableDictionary.CreateRange<string, string>(
+            new List<KeyValuePair<string, string>> {
+                new KeyValuePair<string, string>("Name", product.TryGetAttributeAsString("Name", out var name) ? name : productId),
+                new KeyValuePair<string, string>("Id", product.id().ToString()),
+                new KeyValuePair<string, string>("Guid", productId),
+                new KeyValuePair<string, string>("Type", product.is_a())
+                }
+            );
+        
+        var psets = elem_instance.GetPsets();
+        
+        var matrix = elem.transformation().matrix().data();
+        
+        
+        if (matrix.Count != 12 && matrix.Count != 16) {
+            continue;
+        }
+
+        TriangulationElement shape = elem.TryGetAsTriangulationElement();
+        if (shape == null) {
+            continue;
+        }
+        var geometry = shape.geometry();
+        var shapeId = shape.id();
+        // unique id for the geometry _intended_ to be used for caching/instancing of the mesh
+        var geometryId = geometry.id();
+        
+        // choose our own ids, since the ones supplied by ifcopenshell do not seem to be unique
+        var materials = geometry.materials();
+        
+        
+        if (iterator.next() == null) 
+            break;
+    }
+    
+    
+    //Debug.Log($"Processed {vertices.Count} vertices in the model");
+}
+Console.WriteLine($"Everything took {watch.Elapsed}");
+
+
 
 
 
